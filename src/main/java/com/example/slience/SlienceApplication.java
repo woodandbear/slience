50e8bf16017f27ae8286d9ad39af55f6b656283c
woodandbear <719126493@qq.com>
2023-02-17 00:21:43 +0800

    1.0

---
+++ src/main/java/com/example/slience/SlienceApplication.java
@@ -0,0 +1,13 @@
+package com.example.slience;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlienceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlienceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/slience/SlienceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slience;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlienceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
