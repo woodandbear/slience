a6fffdcdb8b5a0569ebd11f1b695e07b48b82317
woodandbear <719126493@qq.com>
2024-04-08 20:52:38 +0800

    test1.0

---
+++ src/main/java/com/example/slience/controller/Test.java
@@ -0,0 +1,16 @@
+package com.example.slience.controller;
+
+import com.sun.org.apache.xml.internal.res.XMLErrorResources_tr;
+
+/**
+ * @author woodbear
+ * @create 2024-04-08 20:46
+ */
+public class Test {
+
+    public void test(){
+        System.out.println("1111");
+
+    }
+
+}
